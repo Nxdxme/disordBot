fe0627dd74589e51e0535312911009477962b406
Kritsakorn Sataratpayoon <zxcnadeems@gmail.com>
2023-03-08 23:01:27 +0700

    Deploy the commands

---
+++ src/commands/debug/ping.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { command } from '../../utils'
+
+const meta = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('ping the bot for a response.')
+    .addStringOption((option) => 
+        option
+            .setName('message')
+            .setDescription('Provide the bot message to respond with.')
+            .setMinLength(1)
+            .setMaxLength(2000)
+            .setRequired(false)
+    )
+
+export default command(meta, ({ interaction }) => {
+    const msg = interaction.options.getString('message')
+
+    return interaction.reply({
+        ephemeral: true,
+        content: msg ?? 'Pong!🏓'
+    })
+})
